95b369980fc04d74d15593680b02b69fdfd04db1
Vinicius Goneli <viniciusgoneli_2012@hotmail.com>
2020-05-02 14:45:40 -0300

    BoardException and defensive programming

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -4,6 +4,7 @@ public class Position {
 	
 	private int row;
 	private int column;
+	
 	public Position(int row, int column) {
 		super();
 		this.row = row;
---
+++ src/chess/BoardException.java
@@ -0,0 +1,9 @@
+package chess;
+
+public class BoardException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+
+	public BoardException(String msg) {
+		super(msg);
+	}
+}
